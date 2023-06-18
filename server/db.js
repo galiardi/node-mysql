84b5a46a648fddb7b8f89d677beca93afe1e1c7f
galiardi <galiardi.dev@gmail.com>
2023-06-18 13:57:56 -0400

    Add environment variables

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 import { createPool } from 'mysql2/promise';
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+} from './config.js';
 
 export const pool = createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'asdf',
-  database: 'firstone',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_DATABASE,
 });
